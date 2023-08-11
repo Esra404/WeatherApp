3b3248f264c7868cd96f7b1033cf4f827c1d2b38
Esra404 <esradrmz571@gmail.com>
2023-08-11 11:45:35 +0300

    hava durumu api bağlama

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+const bodyParser = require("body-parser");
 const express=require("express");
 const app=express();
+const fetch=require("node-fetch");
+const bodyParser=require("body-parser");
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",(req,res)=>{
-	res.send("hello world");
+	res.sendFile(__dirname+"/index.html");
 });
+app.post("/",async(req,res)=>{
+	let location=await req.body.city;
+	const url=`https://api.openweathermap.org/data/2.5/weather?lat=${location}&lon={lon}&appid=${process.env}`;
+    const response=await fetch(url);
+	const weatherData=response.json();
+	console.log(weatherData);
+})
 app.listen(3000,()=>{
 	console.log("server is running....");
 });
