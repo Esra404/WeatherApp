ee5cfccec61133f0c6422ab662438ed0241cd725
Esra404 <esradrmz571@gmail.com>
2023-08-11 13:07:58 +0300

    env gizleme

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
-const express = require("express");
+const express=require("express");
 const bodyParser = require("body-parser");
+const app=express();
 const fetch = require("node-fetch");
-const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({extended:true}));
 require('dotenv').config();
-
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+app.get("/",(req,res)=>{
+	res.sendFile(__dirname+"/index.html");
 });
-
-app.post("/", async (req, res) => {
-    let location = req.body.city;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.APIKEY}`;
-    try {
-        const response = await fetch(url);
-        const weatherData = await response.json();
-        console.log(weatherData);
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-app.listen(3000, () => {
-    console.log("server is running....");
+app.post("/",async(req,res)=>{
+	let location=await req.body.city;
+	const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.APIKEY}`;
+    const response=await fetch(url);
+	const weatherData=await response.json();
+	console.log(weatherData);
+})
+app.listen(3000,()=>{
+	console.log("server is running....");
 });
